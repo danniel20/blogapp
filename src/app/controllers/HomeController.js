d5477bd21fc8b2d582b63bfe13d3fc685e666f0b
Danniel <danniel.master@gmail.com>
2020-04-28 13:13:11 -0300

    função para criar usuário admin

--- src/app/controllers/HomeController.js
+++ src/app/controllers/HomeController.js
@@ -1,4 +1,5 @@
 const Post = require('../models/Post')
+const User = require('../models/User')
 
 module.exports = {
 
@@ -10,6 +11,26 @@ module.exports = {
     catch(err){
       req.flash("error_msg", "Erro ao listar posts")
       res.redirect('/')
+    }
+  },
+
+  async createUserAdmin(req, res){
+    try{
+      const user = new User({
+        name: 'Admin',
+        email: 'admin@admin.com',
+        password: '123456',
+        isAdmin: 1
+      })
+
+      user.save()
+
+      req.flash("success_msg", `Usuário Admin: { email: ${user.email}, senha: ${user.password}}`)
+      res.redirect("/")
+    }
+    catch(err){
+      req.flash("error_msg", "Erro interno")
+      res.redirect("/")
     }
   }
 
