57e0b664f0b2e05eba4eb62c4a43d08535522d01
Danniel <danniel.master@gmail.com>
2020-04-28 18:51:48 -0300

    ajuste

--- src/config/upload.js
+++ src/config/upload.js
@@ -7,7 +7,6 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     cb(null, `${Date.now()}-${file.originalname}`)
-    // cb(null, `${file.originalname}-${Date.now()}.${path.extname(file.originalname)}`)
   }
 })
 
