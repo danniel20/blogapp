0a8a801f01c12a5b28fef9a18d8e316df69b3db8
Danniel <danniel.master@gmail.com>
2020-05-06 12:22:31 -0300

    ajuste limite upload

--- src/config/upload.js
+++ src/config/upload.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits : {fileSize : 1000000 }, //1MB
+  limits : { fileSize : 1 * 1024 * 1024 }, //1MB
   fileFilter (req, file, cb) {
     const filetypes = /jpeg|jpg|png|gif/
     const extname =  filetypes.test(path.extname(file.originalname).toLowerCase())
