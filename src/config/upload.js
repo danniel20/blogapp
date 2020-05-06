9c08bc596d8a0dc39d65a1a876dede808c8bbd52
Danniel <danniel.master@gmail.com>
2020-05-06 18:18:32 -0300

    hash para imagem

--- src/config/upload.js
+++ src/config/upload.js
@@ -1,12 +1,14 @@
 const multer = require('multer')
 const path = require('path')
+const crypto = require('crypto')
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
       cb(null, 'uploads/')
   },
   filename: function (req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`)
+    const hash = crypto.randomBytes(16).toString('hex')
+    cb(null, `${hash}-${file.originalname}`)
   }
 })
 
