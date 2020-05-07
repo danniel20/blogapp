5096b64a9cd388f0f114f5c841d360a1c45b2087
Danniel <danniel.master@gmail.com>
2020-05-07 18:51:04 -0300

    seed file para usuario

---
+++ src/seeds/1-users/UsersData.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    name: "Usuario 1",
+    email: "usuario1@teste.com",
+    password: "123456"
+  },
+  {
+    name: "Usuario 2",
+    email: "usuario2@teste.com",
+    password: "123456"
+  }
+]
