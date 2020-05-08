10be96c5264fdae3ba64912d4e3d0168f48d8d2e
Danniel <danniel.master@gmail.com>
2020-05-08 10:28:16 -0300

    ajuste seed de users

--- src/seeds/1-users/UsersData.js
+++ src/seeds/1-users/UsersData.js
@@ -1,12 +1,25 @@
-module.exports = [
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+
+const users = []
+
+users.push(
   {
-    name: "Usuario 1",
-    email: "usuario1@teste.com",
-    password: "123456"
-  },
-  {
-    name: "Usuario 2",
-    email: "usuario2@teste.com",
-    password: "123456"
+    name: 'Admin',
+    email: 'admin@admin.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: 1
   }
-]
+)
+
+for (let i = 1; i < 10; i++) {
+  users.push(
+    {
+      name: faker.name.firstName(),
+      email: faker.internet.email(),
+      password: bcrypt.hashSync('123456', 10)
+    }
+  )
+}
+
+module.exports = users
